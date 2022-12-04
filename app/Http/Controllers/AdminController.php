dbd30f8781c114983dc816ad632da233a0ba43f6
Alifa Hafida Anwar <alifahafida@gmail.com>
2022-12-04 14:32:26 +0700

    edit controller admin index

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -34,7 +34,7 @@ class AdminController extends Controller
             'lokers' => $response->vacancy,
             'reports' => $response->report,
             'companies' => $response->company,
-            'events' => $response->events,
+            'events' => $response->event,
             'registration' => $response->regEvent
         ]);
     }
